940e236586974ae7cbe4824e16fd88ad3d7259b3
venubabu <velpurivenubabu123@gmail.com>
2023-05-11 07:14:34 +0530

    [Venubabu] Done With Welcome Message

---
+++ Genrics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Genrics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcom To Genrics Problems!");
+        }
+    }
+}
