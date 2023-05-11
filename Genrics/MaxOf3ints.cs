f9dad0d1147866d0f4f5f95574eb0e0ca17fbb6a
venubabu <velpurivenubabu123@gmail.com>
2023-05-11 07:49:40 +0530

    [Venubabu] Done With integers

---
+++ Genrics/MaxOf3ints.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genrics
+{
+    internal class MaxOf3ints
+    {
+        public static void MaxmumIntegerNumber(int firstValue,int secondValue,int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue)>0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0 )
+            { 
+                Console.WriteLine(firstValue);
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine(secondValue);
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                Console.WriteLine(thirdValue);
+            }
+            
+        }
+
+    }
+}
--- Genrics/Program.cs
+++ Genrics/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcom To Genrics Problems!");
+            Console.WriteLine("Enter Num1");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Num2");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Num3");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            MaxOf3ints.MaxmumIntegerNumber(firstNumber, secondNumber, thirdNumber);
         }
     }
 }
