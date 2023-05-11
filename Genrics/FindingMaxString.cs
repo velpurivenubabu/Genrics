6faf0384e0034d70783f1a6bee9a5d7c032485ec
venubabu <velpurivenubabu123@gmail.com>
2023-05-11 11:35:22 +0530

    [Venubabu] Done With Max Strings

---
+++ Genrics/FindingMaxString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genrics
+{
+    internal class FindingMaxString
+    {
+        public static void FindingMaxSt(string st1,string st2,string st3) {
+            if (st1.CompareTo(st2) > 0 && st1.CompareTo(st3) > 0 ||
+                st1.CompareTo(st2) >= 0 && st1.CompareTo(st3) > 0 ||
+                st1.CompareTo(st2) > 0 && st1.CompareTo(st3) >= 0)
+            {
+                Console.WriteLine(st1);
+            }
+            if (st2.CompareTo(st1) > 0 && st2.CompareTo(st3) > 0 ||
+                st2.CompareTo(st1) >= 0 && st2.CompareTo(st3) > 0 ||
+                st2.CompareTo(st1) > 0 && st2.CompareTo(st3) >= 0)
+            {
+                Console.WriteLine(st2);
+            }
+            if (st3.CompareTo(st1) > 0 && st3.CompareTo(st2) > 0 ||
+               st3.CompareTo(st1) >= 0 && st3.CompareTo(st2) > 0 ||
+               st3.CompareTo(st1) > 0 && st3.CompareTo(st2) >= 0)
+            {
+                Console.WriteLine(st3);
+            }
+
+        }
+    }
+}
--- Genrics/Program.cs
+++ Genrics/Program.cs
@@ -4,21 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcom To Genrics Problems!");
-            Console.WriteLine("Enter Num1");
-            int firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Num2");
-            int secondNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Num3");
-            int thirdNumber = Convert.ToInt32(Console.ReadLine());
-            MaxOf3ints.MaxmumIntegerNumber(firstNumber, secondNumber, thirdNumber);
-            Console.WriteLine("Enter Num1");
-            float firstNumber1 = (float)Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter Float Num2");
-            float secondNumber2 = (float)Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter Float Num3");
-            float thirdNumber3 = (float)Convert.ToDouble(Console.ReadLine());
-            MaxFloats.MaxmumFloatNumber(firstNumber1, secondNumber2, thirdNumber3);
+            Console.WriteLine("Please Enter Any Below Option");
+            Console.WriteLine("1.Finding Max Integer\n2.Finding Max Float\n3.Finding Max String");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Hello, Welcom To Genrics Problems!");
+                    Console.WriteLine("Enter Num1");
+                    int firstNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Num2");
+                    int secondNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Num3");
+                    int thirdNumber = Convert.ToInt32(Console.ReadLine());
+                    MaxOf3ints.MaxmumIntegerNumber(firstNumber, secondNumber, thirdNumber);
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Enter Num1");
+                    float firstNumber1 = (float)Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter Float Num2");
+                    float secondNumber2 = (float)Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter Float Num3");
+                    float thirdNumber3 = (float)Convert.ToDouble(Console.ReadLine());
+                    MaxFloats.MaxmumFloatNumber(firstNumber1, secondNumber2, thirdNumber3);
+                    break;
+                case 3:
+
+                    Console.WriteLine("Enter Num1");
+                    string st1 = Console.ReadLine();
+                    Console.WriteLine("Enter Float Num2");
+                    string st2 = Console.ReadLine();
+                    Console.WriteLine("Enter Float Num3");
+                    string st3 = Console.ReadLine();
+                    FindingMaxString.FindingMaxSt(st1, st2, st3);
+                    break;
+
+            }
+            
+                
+               
+             
+           
+            
         }
+   
     }
 }
